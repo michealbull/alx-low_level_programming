960530b8aeca408442f0e06ba5fc242a3a1998f8
micheal <micheal.young5000@gmail.com>
2022-09-08 11:19:12 -0700

    added puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - entry point
+ *
+ * description: A simple c program that prints with puts function
+ *
+ * return: Always return 0 (Success)
+ */
+
 int main(void)
 {
 	puts("Programming is like building a multilingual puzzle\n");
