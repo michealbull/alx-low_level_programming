0304872503c59ad59405b8d83e3931965d49daa2
micheal <micheal.young5000@gmail.com>
2022-09-09 06:35:02 -0700

    added 6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry piont
+ *
+ * Description: A c code to show sizeof
+ *
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("size of a char: %d\n", sizeof(char));
+	printf("size of an int: %d\n", sizeof(int));
+	printf("size of a long int: %d\n", sizeof(long int));
+	printf("size of a long long: %d\n", sizeof(long long));
+	printf("size of a float: %d\n", sizeof(float));
+	return (0);
