81e45db4c2a43234354a8f37e9151707ac1f1af5
micheal <micheal.young5000@gmail.com>
2022-09-08 23:15:43 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * description: a simple c program that prints with printf
+ *
+ * Return: Always return 0 (Success)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
