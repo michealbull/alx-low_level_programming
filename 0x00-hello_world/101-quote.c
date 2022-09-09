2e942ce2f6b2d48e44a7f666b650ab6f2795e720
micheal <micheal.young5000@gmail.com>
2022-09-09 07:59:33 -0700

    added juju

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: A code with a wrong return
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
