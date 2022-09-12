57623ea5fdd07f22f6ae442f3e7f7edc5a2a6afa
micheal <micheal.young5000@gmail.com>
2022-09-12 12:34:13 -0700

    added 2-print_alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: printing lowercase letters with putchar
+ *
+ * Result: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(a - z);
+	putchar(\n);
+
+	return (0);
+}
