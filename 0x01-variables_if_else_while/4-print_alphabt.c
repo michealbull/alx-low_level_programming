066afc1686c429b6b2b987043094fa25e03765b6
micheal <micheal.young5000@gmail.com>
2022-09-12 16:18:01 -0700

    added without e & q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description: A program that will print letters 
+ * apart from qn and e 
+ *
+ * Return: Always 0 
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch == 'q' || ch == 'e')
+			++ch;
+		putchar(ch);
+		++ch;
+	}
+	puchar('\n');
+
+	return (0);
+}
