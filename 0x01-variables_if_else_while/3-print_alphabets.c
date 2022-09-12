e9d5f76fbf52386eb209018b73f32823de80be73
micheal <micheal.young5000@gmail.com>
2022-09-12 15:43:27 -0700

    added upercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: A program that prints both upper and lower case letters
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	char CH = 'A';
+
+	while (ch <=z)
+	{
+		putchar(ch);
+		++ch;
+	}
+	while (CH <=Z)
+	{
+		putchar(CH);
+		++CH;
+		purchar(\n);
+	}
+	return (0);
+}
