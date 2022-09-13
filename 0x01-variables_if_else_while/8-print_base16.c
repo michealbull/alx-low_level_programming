6bf4b12fb83ec48637f0f622bc19ae11210aed9b
micheal <micheal.young5000@gmail.com>
2022-09-13 05:42:08 -0700

    added 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{ 	int  ch = 48;
+
+	while (ch <= 102)
+	{ 
+		putchar(ch + '0')
+			if (ch = 57)
+			{
+				ch += 39;
+			}
+	}
+	putchar('\n');
+
+	return (0);
+}
