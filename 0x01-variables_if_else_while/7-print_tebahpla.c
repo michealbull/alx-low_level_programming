7728ce086a66fddbdc814f25087e65cded5ef648
KOSISOCHUKWU MICHEAL <82963252+michealbull@users.noreply.github.com>
2022-09-13 04:55:21 -0700

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that will print reverse
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (ch >= 'a')
+	{
+		putchar(ch);
+		--ch;
+	}
+	putchar('\n');
+
+	return (0);
+}
