dbf07666fd817020e585e7d96d747dfb6c463bb3
micheal <micheal.young5000@gmail.com>
2022-09-14 05:01:43 -0700

    started light

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char str[8] = "_putchar";
+	int ch = 
+	
+	while (ch < 9)
+	{
+		_putchar(str[ch]);								ch++;
+											}
+	_putchar('\n');
+	return (0);
+}
