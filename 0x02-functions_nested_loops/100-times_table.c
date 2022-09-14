3556f9941389008feff54d26d0d624dc898e38d7
KOSISOCHUKWU MICHEAL <82963252+michealbull@users.noreply.github.com>
2022-09-14 15:58:42 -0700

    Create 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,41 @@
+#include"main.h"
+
+/**
+ * print_times_table - prints time table of n
+ *
+ * @n: takes number input
+*/
+
+void print_times_table(int n)
+{
+	int jam, fish, cim;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (cim = 0; cim <= n; ++cim)
+		{
+			_putchar(48);
+			for (fish = 1; fish <= n; ++fish)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				jam = cim * fish;
+
+				if (jam <= 9)
+					_putchar(' ');
+				if (jam <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((jam / 100) + 48);
+					_putchar((jam / 10) % 10 + 48);
+				} else if (jam <= 99 && jam >= 10)
+					_putchar((jam / 10) + 48);
+				_putchar((jam % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
