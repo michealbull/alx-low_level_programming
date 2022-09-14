8356ddb0a5d0f9525c67f4603178f4bd2777e81e
micheal <micheal.young5000@gmail.com>
2022-09-14 08:28:47 -0700

    added void print_alphabet_x10(void)

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+	int d;
+	for (d = 0;d <= 10; ++d)
+	{
+		int ch;
+		for (ch = 'a'; ch <= z; ++ch)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
