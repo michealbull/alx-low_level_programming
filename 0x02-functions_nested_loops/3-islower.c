fee4aa6f3f6fd555e99e8448a26b8cef0d7c178d
micheal <micheal.young5000@gmail.com>
2022-09-14 08:56:03 -0700

    added int _islower(int c)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include"main"
+
+/**
+ * islower - ensures that c is lower case
+ *
+ * Return: Always 0
+ */
+
+int _islower(int c)
+{
+	int c;
+
+	if (c >= 97 && c <= 122)
+		return (1);
+	
+	return (0);
+}
