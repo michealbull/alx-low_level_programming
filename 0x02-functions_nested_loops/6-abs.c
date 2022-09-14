cc0457c3765e6ce829ade4f7e87d2028b550799b
micheal <micheal.young5000@gmail.com>
2022-09-14 13:56:55 -0700

    added #includemain.h

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include"main.h"
+
+/**
+ *
+ * _abs - absolute value of an integer
+ *
+ * @n: will be given
+ *
+ * Return: return n
+ *
+ */
+
+int _abs(int n)
+
+{
+
+	if (n < 0)
+
+	n = (-1) * n;
+
+	return (n);
+
+}
