7671485c6e39d36d1f768bced3eb81bb35913263
micheal <micheal.young5000@gmail.com>
2022-09-14 11:50:57 -0700

    added int print_sign(int n)

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include"main.h"
+
+/**
+ * print_sign - printing signs
+ *
+ * @n: that's already defined
+ *
+ * Return: return 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(+);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	else (n < 0)
+	{
+		_putchar(-);
+		return (-1);
+	}
+}
+
