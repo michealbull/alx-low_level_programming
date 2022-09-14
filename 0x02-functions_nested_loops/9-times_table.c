a1906ce3683908d79e03bdb80369b3a8ea4c764b
KOSISOCHUKWU MICHEAL <82963252+michealbull@users.noreply.github.com>
2022-09-14 15:20:35 -0700

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * times_table - print multiplication table
+ *
+*/
+
+void times_table(void)
+{
+	int chim, fish, jam;
+
+	for (chim = 0; chim <= 9; ++chim)
+	{
+		_putchar('0');
+		for (fish = 1; fish <= 9; ++fish)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			jam = chim * fish
+        
+			if (jal <= 9)
+				_putchar(' ');
+			else
+				_putchar((jam / 10) + 48);
+
+			_putchar((jam % 10) + 48); 
+		}
+		_putchar('\n');
+	}
+}
