e2e164fd2835d8c6f6d57fff6c85890a1206d440
micheal <micheal.young5000@gmail.com>
2022-09-14 07:49:12 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include"main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+{
+	char d;
+
+	for (d = 'a'; d <= 'z'; ++d)
+	{
+		_putchar(d);
+	}
+	_putchar('\n');
+}
