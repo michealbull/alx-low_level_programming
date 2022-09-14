d6b3e348c6840b53a834202eb8154dfb527f439e
micheal <micheal.young5000@gmail.com>
2022-09-14 14:09:59 -0700

    int print_last_digit(int)

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * Return: return 0
+ */
+
+int print_last_digit(int)
+{
+	r;
+
+	r % 10;
+
+	_putchar(r);
+	_putchar('\n');
+
+	return (0);
+}
