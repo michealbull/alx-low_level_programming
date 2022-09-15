8db5d4e7a24acf32eaab93be459e75a537cca7b1
micheal <micheal.young5000@gmail.com>
2022-09-15 04:06:56 -0700

    added few

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * mul - Entry point
+ *
+ * @a: takes first input
+ * @b:  takes second input
+ *
+ * Return: return f
+ */
+
+int mul(int a, int b)
+{
+	int f;
+	
+	f = a * b;
+	
+	return (f);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * print_numbers - Entry point 
+ *
+ */
+
+void print_numbers(void)
+{
+	int d;
+
+	for (d = 0; d < 10; ++d)
+		_putchar(d);
+	_putchar('\n');
+}
