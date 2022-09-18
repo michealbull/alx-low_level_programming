c0ddf691355a64b36089c1276c9dc4db48515143
KOSISOCHUKWU MICHEAL <82963252+michealbull@users.noreply.github.com>
2022-09-18 05:07:31 -0700

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,18 @@
 #include"main.h"
 
 /**
- * print_numbers - Entry point
- *
+ * print_numbers - print 0 - 9
+ * 
+ * Return: 0
  */
 
 void print_numbers(void)
 {
-	int d;
+	int num = 0;
 
 	do {
-		_putchar(d);
-	} while (d = 0,++d);
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
 	_putchar('\n');
 }
