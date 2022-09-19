1782f7f5960bc2c982aa049521f8b913f1d1c5ca
micheal <micheal.young5000@gmail.com>
2022-09-19 13:37:26 -0700

    added puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include"main.h"
+#include <stdio.h>
+
+/**
+ * _puts - Entry point
+ *
+ * @str: takes inputs
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	for (str = 0; *str != '\0'; ++str)
+	{
+		_putchar(*str);
+		_putchar('\n');
+	}
+}
