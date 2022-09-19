dfc750e1007622eb0eed66b04993581399c4babd
micheal <micheal.young5000@gmail.com>
2022-09-19 13:21:08 -0700

    added strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,24 @@
+#include"main.h"
+#include<stdio.h>
+
+/**
+ * _strlen - entry point
+ *
+ * @s: takes input
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	char *str;
+
+	int len;
+
+
+
+	str = "My first strlen!";
+
+	len = _strlen(str);
+	return(strlen);
+}
