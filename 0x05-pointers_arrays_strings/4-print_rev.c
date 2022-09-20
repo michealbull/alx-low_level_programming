5c60d7b1bb4449b133125eba0957d09a2ee799e7
micheal <micheal.young5000@gmail.com>
2022-09-20 04:27:19 -0700

    adde print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include"main.h"
+
+/**
+ * print_rev - Entry point
+ *
+ * @s: takes input
+ *
+ * Return: Always 0
+ */
+
+void _print_rev(char *s)
+{
+	int sin;
+
+	for (sin = 0; sin != '\0'; sin++)
+		for (sin = sin - 1; sin >= 0; sin--)
+			_putchar(s[sin]);
+	_putchar('\n');
+}
