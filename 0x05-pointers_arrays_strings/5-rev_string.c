e3aed9225a57ce36288564fd6ddda5cdf67d4413
KOSISOCHUKWU MICHEAL <82963252+michealbull@users.noreply.github.com>
2022-09-20 13:21:36 -0700

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+/**
+ * rev_string - reverses a string
+ *
+ * @s: takes input
+ *
+ * Return: Nothing
+ */
+
+{
+	int l, i;
+	char ch;
+
+	for (l = 0; s[l] != '\0'; ++l)
+		;
+
+	for (i = 0; i < l / 2; ++i)
+	{
+		ch = s[i];
+		s[i] = s[l - 1 - i]; 
+		s[l - 1 - i] = ch;
+	}
+
+}
